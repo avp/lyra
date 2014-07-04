fe5b24fcd8d2c9cd7dc7acfeb4ee7429aff6ca40
Aakash Patel <aakpat6@gmail.com>
2014-07-04 10:34:43 -0700

    Add password to GET /reset.

--- routes/reset.js
+++ routes/reset.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
 var fs = require('fs');
+var Crypto = require('cryptojs').Crypto;
+var basicAuth = require('basic-auth-connect');
 
-router.get('/', function(req, res) {
+router.get('/', basicAuth(function(user, pass) {
+  return user === 'admin' && Crypto.MD5(pass).toString() === '7b69b90521d0c3a81c61395387820ab4';
+}), function(req, res) {
   var MongoClient = require('mongodb').MongoClient;
 
   MongoClient.connect('mongodb://127.0.0.1:27017/musicsort', function(err, db) {
@@ -38,7 +42,6 @@ router.get('/', function(req, res) {
           };
         }).value();
 
-        console.log(songs);
         collection.insert(songs, function(err) {
           if (err) {
             throw err;
